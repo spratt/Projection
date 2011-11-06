17ab9fc7358872ff873bf151f9c36d09e5f0ee7f
Simon David Pratt <me@SimonDavidPratt.com>
2011-11-06 15:28:58 -0500

    Now drawing object in planes

--- main.js
+++ main.js
@@ -11,3 +11,13 @@
 * NOTES:   This is the code specific to the proof of concept.                 *
 *                                                                             *
 ******************************************************************************/
+
+function main() {
+    var p = new DrawableObject(10,10,10,"ship.png");
+    var xz_plane = document.getElementById("xz_plane");
+    var yz_plane = document.getElementById("yz_plane");
+    p.draw_yz(yz_plane);
+    p.draw_xz(xz_plane);
+}
+
+window.onload=main;
--- projection.js
+++ projection.js
@@ -11,3 +11,47 @@
 * NOTES:   This is the library file to include when using projection.         *
 *                                                                             *
 ******************************************************************************/
+
+function DrawableObject(x,y,z,img_src) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.img_src = img_src;
+    this.planes = [];
+    this.images = [];
+}
+
+function inArray(item,array) {
+    for(var i in array) {
+	if(array[i] == item) return i;
+    }
+    return -1;
+}
+
+DrawableObject.prototype.draw_xz = function(xz_plane) {
+    console.log("Drawing xz...");
+    var i = inArray(xz_plane,this.planes);
+    if(i < 0) {
+	var img = new Image();
+	img.src = this.img_src;
+	xz_plane.appendChild(img);
+	i = this.planes.length;
+	this.planes[i] = xz_plane;
+	this.images[i] = img;
+    }
+    console.log("Done drawing xz.");
+}
+
+DrawableObject.prototype.draw_yz = function(yz_plane) {
+    console.log("Drawing yz...");
+    var i = inArray(yz_plane,this.planes);
+    if(i < 0) {
+	var img = new Image();
+	img.src = this.img_src;
+	yz_plane.appendChild(img);
+	i = this.planes.length;
+	this.planes[i] = yz_plane;
+	this.images[i] = img;
+    }
+    console.log("Done drawing yz.");
+}
